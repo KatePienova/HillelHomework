ab2c3604046aa0f88c1b547b940ac158c5bb4e5c
Niki4570 <nkrivoruchko125133@gmail.com>
2022-08-02 15:04:04 +0300

    UPD 2

--- src/com/hw1/Main.java
+++ src/com/hw1/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main (String[] args){
         System.out.print("Hello world");
+        System.out.print("Java Pro");
+        System.out.print("Hello Eugeniya!");
+
     }
 }
