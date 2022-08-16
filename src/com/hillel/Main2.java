0b8f2a85b6394703739096d3827ed6bee5fa9e7f
KatePienova <104775894+KatePienova@users.noreply.github.com>
2022-08-16 19:10:12 +0300

    adding files

--- src/com/hillel/Main2.java
+++ src/com/hillel/Main2.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Main {
-
+public class Main2 {
     public static void main(String[] args) {
         System.out.println("Java first homework");
+        System.out.println("Java first commit");
     }
 }
